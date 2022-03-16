6305ce48065c2ee7319c0bcd218770bfc763afb3
Juan G. Molina <jg.molina92@hotmail.com>
2022-03-16 13:32:31 -0500

    Move player controls to component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, {useRef} from 'react';
 import './App.css';
+import VideoPlayerControls from './components/VideoPlayerControls'
 
 import video from "./assets/video.mp4";
 import useVideoPlayer from './hooks/useVideoPlayer';
 
 const App = () => {
   const videoElement = useRef(null);
-  const {
-    playerState,
-    togglePlay,
-    handleOnTimeUpdate,
-    handleVideoProgress,
-    handleVideoSpeed,
-    toggleMute,
-  } = useVideoPlayer(videoElement);
+  const { playerState, handleOnTimeUpdate } = useVideoPlayer(videoElement);
 
   return (
     <div className="container">
@@ -23,41 +17,7 @@ const App = () => {
           ref={videoElement}
           onTimeUpdate={handleOnTimeUpdate}
         />
-        <div className="controls">
-          <div className="actions">
-            <button onClick={togglePlay}>
-              {!playerState.isPlaying ? (
-                <i className="bx bx-play"></i>
-              ) : (
-                <i className="bx bx-pause"></i>
-              )}
-            </button>
-          </div>
-          <input
-            type="range"
-            min="0"
-            max="100"
-            value={playerState.progress}
-            onChange={(e) => handleVideoProgress(e)}
-          />
-          <select
-            className="velocity"
-            value={playerState.speed}
-            onChange={(e) => handleVideoSpeed(e)}
-          >
-            <option value="0.50">0.50x</option>
-            <option value="1">1x</option>
-            <option value="1.5">1.5x</option>
-            <option value="2">2x</option>
-          </select>
-          <button className="mute-btn" onClick={toggleMute}>
-            {!playerState.isMuted ? (
-              <i className="bx bxs-volume-full"></i>
-            ) : (
-              <i className="bx bxs-volume-mute"></i>
-            )}
-          </button>
-        </div>
+        <VideoPlayerControls videoElement={videoElement} progress={playerState.progress}/>
       </div>
     </div>
   );
