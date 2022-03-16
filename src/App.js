ce7754267e2db340c418218d0da53d4b7b0419f3
Juan G. Molina <jg.molina92@hotmail.com>
2022-03-15 19:02:24 -0500

    Add container and video with initial styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
+import video from "./assets/video.mp4";
+
 function App() {
   return (
-    <>
-    </>
+    <div className="container">
+      <div className="video-wrapper">
+        <video src={video}/>
+      </div>
+    </div>
   );
 }
 
