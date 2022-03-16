3145c01a2915ec3e41ff3fb63d6b30391966399f
Juan G. Molina <jg.molina92@hotmail.com>
2022-03-15 20:29:31 -0500

    Add video player controls and their styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ function App() {
     <div className="container">
       <div className="video-wrapper">
         <video src={video}/>
+        <div className="controls">
+          <div className="actions">
+            <button>
+              <i className="bx bx-play"></i>
+            </button>
+          </div>
+          <input type="range" min="0" max="100"/>
+          <select className="velocity">
+            <option value="0.50">0.50x</option>
+            <option value="1">1x</option>
+            <option value="1.5">1.5x</option>
+            <option value="2">2x</option>
+          </select>
+          <button className="mute-btn">
+            <i className="bx bx-volume-full"></i>
+          </button>
+        </div>
       </div>
     </div>
   );
