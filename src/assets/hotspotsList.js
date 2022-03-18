b449d20ad68d5e3bf754a89fb44c3c66007ef82d
Juan G. Molina <jg.molina92@hotmail.com>
2022-03-17 23:56:16 -0500

    Modify hotspotsList content and change video to one with more movement

--- src/assets/hotspotsList.js
+++ src/assets/hotspotsList.js
@@ -1,19 +1,24 @@
 const hotspotsList = [
   {
     id: 1,
-    time: 5,
-    text: 'Hotspot 1'
+    time: 2,
+    text: 'Lorem ipsum dolor sit amet consectetur adipiscing, elit feugiat nisi magna praesent habitant ut, ridiculus id vulputate auctor dictum.'
   },
   {
     id: 2,
     time: 12,
-    text: 'Hotspot 2'
+    text: 'Gravida est tempor in hendrerit class augue curae eros, non nullam tristique primis odio faucibus condimentum habitasse euismod, erat natoque volutpat suscipit aliquam hac morbi. Ad quisque erat auctor morbi inceptos quam nisi lectus fames.'
   },
   {
     id: 3,
     time: 17,
-    text: 'Hotspot 3'
-  }
+    text: 'feugiat pellentesque class vestibulum sagittis aenean nunc duis, rutrum neque eu nascetur condimentum fermentum praesent consequat.'
+  },
+  {
+    id: 4, // This will not be rendered
+    time: 25,
+    text: 'feugiat pellentesque class vestibulum sagittis aenean nunc duis, rutrum neque eu nascetur condimentum fermentum praesent consequat.'
+  },
 ];
 
 export default hotspotsList;
